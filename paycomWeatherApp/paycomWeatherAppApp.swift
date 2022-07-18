1225452c73ac30f76c755b1a38d8aad8b78c8929
Josh Quaid <josh.quaid@gmail.com>
2022-07-18 12:04:52 -0500

    Initial Commit

---
+++ paycomWeatherApp/paycomWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  paycomWeatherAppApp.swift
+//  paycomWeatherApp
+//
+//  Created by Josh Quaid on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct paycomWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
